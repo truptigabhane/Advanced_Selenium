027c108c340917d0954dff88271c660ce6fb08a6
trivedi7281 <trivedi7281@outlook.com>
2022-08-12 13:40:52 +0530

    Created Classes

---
+++ src/main/java/org/example/pageObject/HomePageObjects.java
@@ -0,0 +1,10 @@
+package org.example.pageObject;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePageObjects {
+
+//    @FindBy(id = "firstname");
+//    WebElement txt_firstname;
+}
